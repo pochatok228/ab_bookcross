3947a5495299ef6693e7cdd6f7b50b3b33460419
pochatok228 <madrigal.2014@mail.ru>
2020-05-29 00:35:35 +0300

    0.0.0.1

---
+++ PROJECT/New Unity Project/Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    public float CameraMovementPerFrame = 0.5f;
+    public int CameraBorder = 50;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    private Vector3 GetVector()
+    {
+        Vector3 CameraMovementVector = new Vector3();
+
+        if (Input.GetKey(KeyCode.W) && transform.position.z < CameraBorder) CameraMovementVector += new Vector3(0, 0, CameraMovementPerFrame);
+        if (Input.GetKey(KeyCode.S) && transform.position.z > -1 * CameraBorder) CameraMovementVector += new Vector3(0, 0, -1 * CameraMovementPerFrame);
+        if (Input.GetKey(KeyCode.A) && transform.position.x > -1 * CameraBorder) CameraMovementVector += new Vector3(-1 * CameraMovementPerFrame, 0, 0);
+        if (Input.GetKey(KeyCode.D) && transform.position.x <  CameraBorder) CameraMovementVector += new Vector3(CameraMovementPerFrame, 0, 0);
+        return CameraMovementVector;
+    }
+
+    private void FixedUpdate()
+    {
+        transform.Translate(GetVector());
+    }
+}
---
+++ PROJECT/New Unity Project/Assets/Scripts/CameraZoom.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraZoom : MonoBehaviour
+{
+
+    public float CameraZoomPerFrame = 0.5f;
+    public float LowerCameraBorder = 15;
+    public float UpperCameraBorder = 50;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 ZoomVector = new Vector3();
+        if (Input.GetKey(KeyCode.Q) && transform.position.y < UpperCameraBorder) ZoomVector += new Vector3(0, CameraZoomPerFrame, -1 * CameraZoomPerFrame);
+        if (Input.GetKey(KeyCode.E) && transform.position.y > LowerCameraBorder) ZoomVector += new Vector3(0, -1 * CameraZoomPerFrame, CameraZoomPerFrame);
+        transform.Translate(ZoomVector);
+    }
+}
